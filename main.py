d0ac6e3506e5877f06cf54a50aeda02af8783f79
Jaques <pedronjaques1@gmail.com>
2022-11-22 22:41:37 +0000

    Movements on going

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
+#import numpy as np
+import random as rand
+
 # Create your objects here - initialize objects
 ev3 = EV3Brick()
 
@@ -14,24 +17,73 @@ left_arm_motor = Motor(Port.A)
 left_leg = Motor(Port.B)
 right_leg = Motor(Port.C)
 
+
 color_sensor = ColorSensor(Port.S1)
+gyro_sensor = GyroSensor(Port.S2)
 right_shoulder = TouchSensor(Port.S3)
 eyes = UltrasonicSensor(Port.S4)
 
 robot = DriveBase(left_leg, right_leg, 25, 105)
 
+#Variables
+DRIVE_DISTANCE = 200
+
+POSSIBLE_PLAYS = np.array(['RECON', 'ATTACK', 'MOVEMENT'])
+POSSIBLE_ATTACKS = np.array(['SHOT', 'STUN'])
+
+matrix_map = np.array([
+    ['Robot',2,3,4,5,6],
+    [1,2,3,4,5,6],
+    [1,2,3,4,5,6],
+    [1,2,3,4,5,6],
+    [1,2,3,4,5,6],
+    [1,2,3,4,5,'Hornet']])
+
 # Write your program here.
 
-robot.drive(200, 0) # changing the speed requires changing the wait on line 31
+def move_front:
+    #verifica a posição na matriz
+    robot.straight(DRIVE_DISTANCE)#movimentar em frente
+    #atualiza a posição na matriz
 
-while eyes.distance() > 100:
+def move_back:
+    robot.straight(-180)
 
-    color = color_sensor.color()
-    print(color)
+def move_left:
+    robot.turn(90)
+    robot.straight(DRIVE_DISTANCE)
 
-    if color == Color.BLACK: # limit of the square reached 
-        wait(380) # amount of time to wait until robot reaches middle of the square in the matrix
-        robot.stop()
-        ev3.speaker.say('MY NAME IS AN TE RO')
-        robot.drive_time(0, 51, 2000)
-        robot.drive(200, 0)
+def move_right:
+    robot.turn(-90)
+    robot.straight(DRIVE_DISTANCE)
+
+def random_movement:
+    possible_movements = np.array(['FRONT', 'BACK', 'RIGHT', 'LEFT'])
+
+    movement = rand.choice(possible_movements)
+
+    match movement:
+        case 'FRONT':
+            move_front()
+        case 'BACK':
+            move_back()
+        case 'RIGHT':
+            move_right()
+        case 'LEFT':
+            move_left()
+        case _:
+            return 0
+
+# Write your program here.
+if right_shoulder.pressed():
+    play = rand.choice(POSSIBLE_PLAYS)
+
+    match play:
+        case 'RECON':
+            return 1
+        case 'ATTACK':
+            return 2
+        case 'MOVEMENT':
+            return 3
+        case _:
+            return 0
